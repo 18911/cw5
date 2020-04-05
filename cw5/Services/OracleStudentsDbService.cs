390d3d8cf597dcaf1050bc405f1115c412e0a96e
unknown <s18911@pjwstk.edu.pl>
2020-04-06 00:07:07 +0200

    cw5

---
+++ cw5/Services/OracleStudentsDbService.cs
@@ -0,0 +1,27 @@
+﻿using cw3.Models;
+using cw5.Models;
+using cw5.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw5.Services{
+    public class OracleStudentsDbService : IStudentDbService
+    {
+        public bool CheckStudiesDb(EnrollStudentRequest enrollStudent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Enrollment GetEnrollment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PromoteStudents(Promotions promotions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
